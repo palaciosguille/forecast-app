4cd30a5614f6934e283c827d9cb60494bef5d1fe
palaciosguille <166257616+palaciosguille@users.noreply.github.com>
2026-07-27 16:31:45 -0400

    Remove baseline from week's max temperatures chart

--- forecast.py
+++ forecast.py
@@ -35,7 +35,6 @@ if "forecast" in data:
     
     st.write("### week's max temperatures")
     chart_data = df.set_index("time")[["temperature_2m_max"]].tail(7)
-    chart_data.loc["baseline"] = 0 
     st.line_chart(chart_data)
 else:
     st.error("⚠️ Weather API block or error occurred.")
