c9bd636fd219f8554051d39fe064b7b2eb101fb7
palaciosguille <166257616+palaciosguille@users.noreply.github.com>
2026-07-27 14:45:55 -0400

    Add files via upload

---
+++ forecast.py
@@ -0,0 +1,50 @@
+import streamlit as st
+import pandas as pd
+import geocoder
+
+
+st.set_page_config(layout="wide")
+
+g = geocoder.ip('me')
+latitude, longitude = g.latlng
+print(latitude)
+print(longitude)
+
+import requests
+
+url = (
+    f"https://api.open-meteo.com/v1/forecast"
+    f"?latitude={latitude}"
+    f"&longitude={longitude}"
+    f"&daily=temperature_2m_max,temperature_2m_min"
+)
+
+data = requests.get(url).json()
+df = pd.DataFrame(data["daily"])
+
+
+
+st.title("Weather Forecast")
+
+st.markdown("#### ") 
+
+st.write("### This week's weather")
+
+st.dataframe(df)
+
+st.markdown("### ") 
+
+st.write("### Representation of this week's weather")
+
+st.bar_chart(df, x="time", y="temperature_2m_max")
+
+st.markdown("### ") 
+
+st.write("### today's max and min temperatures")
+
+st.line_chart(df[["temperature_2m_max", "temperature_2m_min"]])
+
+
+
+
+
